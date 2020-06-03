8cc78f6be76d8e953500d9ba4d70d345601c54ee
mhr <aniimmf868@gmail.com>
2020-06-04 08:25:55 +0900

    add employee new

--- WebContent/WEB-INF/views/topPage/index.jsp
+++ WebContent/WEB-INF/views/topPage/index.jsp
@@ -2,6 +2,45 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <c:import url="../layout/app.jsp">
     <c:param name="content">
-        <h2>日報管理システムへようこそ</h2>
+        <h2>日報 一覧</h2>
+        <table>
+            <tr>
+                <th>社員番号</th>
+                <th>氏名</th>
+                <th>操作</th>
+            </tr>
+            <c:forEach var="employee" items="${employees}" varStatus="status">
+                <tr class="row${status.count % 2}">
+                    <td><c:out value="${employee.code}"/></td>
+                    <td><c:out value="${employee.name}"/></td>
+                    <td>
+                        <c:choose>
+                             <c:when test="${employee.delete_flag == 1}">
+                                 (削除済み)
+                             </c:when>
+                             <c:otherwise>
+                                 <a href="<c:url value='/employees/show?id=${employee.id}'/>">詳細を表示</a>
+                             </c:otherwise>
+                        </c:choose>
+                    </td>
+                </tr>
+            </c:forEach>
+        </table>
+
+        <div id="pagination">
+            (全 ${employees_count} 件) <br />
+            <c:forEach var="i" begin="1" end="${(employees_count - 1) / 15 + 1}" step="1">
+                <c:choose>
+                    <c:when test="${page == i}">
+                        <c:out value="${i}"></c:out>&nbsp;
+                    </c:when>
+                    <c:otherwise>
+                        <a href="${pageContext.request.contextPath}/index?page=${i}"><c:out value="${i}"></c:out></a>
+                    </c:otherwise>
+                </c:choose>
+            </c:forEach>
+        </div>
+
+        <p><a href="<c:url value='/employees/new' />">新規従業員の登録</a></p>
     </c:param>
 </c:import>
--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -1,13 +1,18 @@
 package controllers.toppage;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class TopPageIndexServlet
@@ -29,6 +34,35 @@ public class TopPageIndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		EntityManager em = DBUtil.createEntityManager();
+
+		//urlのパラメータからpage拾う　catch(NumberFormatException e)は数字以外のパラメータの値に対応
+		int page = 1;
+		try {
+			page = Integer.parseInt(request.getParameter("page"));
+		} catch(NumberFormatException e) {}
+
+		List<Employee> employees = em.createNamedQuery("getAllEmployees", Employee.class)
+				                     .setFirstResult(15 * (page - 1))//セット　ページの表示する最初
+				                     .setMaxResults(15)//セット　ページの表示件数
+                                     .getResultList();//ゲット　リスト
+
+		long employees_count = em.createNamedQuery("getEmployeesCount", Long.class)
+				                 .getSingleResult();//ゲット　
+
+		em.close();
+
+		//ページネーションでセットするもの　この３つ
+		request.setAttribute("page", page);
+		request.setAttribute("employees", employees);
+		request.setAttribute("employees_count", employees_count);
+
+		//フラッシュ　セッションスコープだっけ？
+		if(request.getSession().getAttribute("flush") != null) {
+			request.setAttribute("flush", request.getSession().getAttribute("flush"));
+			request.getSession().removeAttribute("flush");
+		}
+
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
         rd.forward(request, response);
 	}
